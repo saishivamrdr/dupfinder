d184a53da27fff26a42ac538e28b753841623a4d
Sai Shivam <24157035@kiit.ac.in>
2026-06-05 11:09:14 +0530

    first end of the file read code done- to be contd

--- src/main/java/com/dupfinder/ByteComparator.java
+++ src/main/java/com/dupfinder/ByteComparator.java
@@ -1,4 +1,28 @@
 package com.dupfinder;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
 
-public class ByteComparator {
+public final class ByteComparator {
+    private static final int BUFFER_SIZE= 8192;
+
+    private ByteComparator() {
+
+    }
+    public static boolean identical(Path first, Path second) throws IOException{
+        if(Files.size(first) != Files.size(second)) {
+            return false;
+        }
+
+        byte[] firstBuffer= new byte[BUFFER_SIZE];
+        byte[] secondBuffer= new byte[BUFFER_SIZE];
+
+        try(InputStream firstInput= Files.newInputStream(first);
+            InputStream secondInput= Files.newInputStream(second)){
+            int firstRead;
+        }
+    }
+
 }
